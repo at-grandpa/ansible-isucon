f59565fb57c3828ab42527bb76a37128ad6152bd
at-grandpa <yoshiyuki.tsuchida.k@gmail.com>
2017-05-06 08:38:04 +0900

    fix env.sh

--- isucon5-qualifier/image/files/env.sh
+++ isucon5-qualifier/image/files/env.sh
@@ -1,2 +1,7 @@
 PATH=/usr/local/bin:/home/isucon/.local/ruby/bin:/home/isucon/.local/node/bin:/home/isucon/.local/python3/bin:/home/isucon/.local/perl/bin:/home/isucon/.local/php/bin:/home/isucon/.local/php/sbin:/home/isucon/.local/go/bin:/home/isucon/.local/scala/bin:$PATH
 GOPATH=/home/isucon/gocode
+ISUCON5_DB_HOST=isucon5q-crystal-mysql
+ISUCON5_DB_PORT=3306
+ISUCON5_DB_USER=root
+ISUCON5_DB_PASSWORD=root
+ISUCON5_DB_NAME=isucon5q
